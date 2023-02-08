37ca4acd36708efa727246bc40a2d1179ce9762e
Osifo John <osifojohntec@gmail.com>
2023-02-08 14:19:29 -0800

    implementig Directory and CreateCommuntyModal

---
+++ src/components/Modal/CreateCommunity/CreateCommunityModal.tsx
@@ -0,0 +1,48 @@
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+} from '@chakra-ui/react';
+import React from 'react';
+
+type CreateCommunityModalProps = {
+  open: boolean;
+  handleClose: () => void;
+};
+
+const CreateCommunityModal: React.FC<CreateCommunityModalProps> = ({
+  open,
+  handleClose,
+}) => {
+  return (
+    <>
+      <Modal blockScrollOnMount={false} isOpen={open} onClose={handleClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text fontWeight="bold" mb="1rem">
+              You can scroll the content behind the modal
+            </Text>
+            {/* <Lorem count={2} /> */}
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Create Community</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+export default CreateCommunityModal;
--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -1,18 +1,19 @@
-import { SearchIcon } from '@chakra-ui/icons';
+import { Search2Icon } from '@chakra-ui/icons';
 import { Flex, Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
+import { User } from 'firebase/auth';
 import React from 'react';
 
 type SearchInputProps = {
-  // user
+  user?: User | null;
 };
 
-const SearchInput: React.FC<SearchInputProps> = () => {
+const SearchInput: React.FC<SearchInputProps> = ({ user }) => {
   return (
-    <Flex flexGrow={1} mr={2} align="center">
+    <Flex flexGrow={1} mr={2} align="center" maxWidth={user ? 'auto' : '600px'}>
       <InputGroup>
         <InputLeftElement
           pointerEvents="none"
-          children={<SearchIcon color="gray.300" mb={1} />}
+          children={<Search2Icon color="gray.300" mb={1} />}
         />
         <Input
           placeholder="Search Reddit"
